72f3ccaf553969efb0427967ebb72ebfdc5c20a4
omar <omar@tzst.com>
2024-01-13 11:38:29 +0100

    fin part_1 peye wuth paypal

--- food/pay.php
+++ food/pay.php
@@ -1,11 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+require "../config/config.php";
+require "../lisb/App.php";
+require "../includes/header.php";?>
+<?php 
+
+
+    if(!isset($_SERVER['HTTP_REFERER'])){
+        // redirect them to your desired location
+        echo "<script>window.location.href='".APPURL."'</script>";
+        exit;
+    }
+
+
+?>
+        <div class="container-xxl py-5 bg-dark hero-header mb-5">
+                <div class="container text-center my-5 pt-5 pb-4">
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">Pay wit PayPal</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center text-uppercase">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Pay</a></li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    <div class="container">  
+                    <!-- Replace "test" with your own sandbox Business account app client ID -->
+                    <script src="https://www.paypal.com/sdk/js?client-id=Aexh9I8Rq-2NkKh6T7BPSi4tdyxVIOnAT_A8kOp-VdVxfU3Cs0WBlBFa-pJgICVTngyex-J2p6nLd95j&currency=USD"></script>
+                    <!-- Set up a container element for the button -->
+                    <div style="margin-left: 200px;" id="paypal-button-container"></div>
+                    <script>
+                        paypal.Buttons({
+                        // Sets up the transaction when a payment button is clicked
+                        createOrder: (data, actions) => {
+                            return actions.order.create({
+                            purchase_units: [{
+                                amount: {
+                                    value: '<?php echo $_SESSION['total_price']; ?>' // Can also reference a variable or function
+                                }
+                            }]
+                            });
+                        },
+                        // Finalize the transaction after payer approval
+                        onApprove: (data, actions) => {
+                            return actions.order.capture().then(function(orderData) {
+                          
+                             window.location.href='delete-cart.php';
+                            });
+                        }
+                        }).render('#paypal-button-container');
+                    </script>
+                  
+               
+        </div>
+<?php require "../includes/footer.php"; ?>
