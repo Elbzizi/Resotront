8ef3ab331d20353083a11f2c44e1ce571a4aa6f5
omar <omar@tzst.com>
2024-02-21 16:41:34 +0100

    modification dans page admin

--- Admin/index.php
+++ Admin/index.php
@@ -6,6 +6,8 @@ $nbfood=$app->SelectOne("SELECT count(*) as num from foods");
 $nborder=$app->SelectOne("SELECT count(*) as num from orders");
 $nbusers=$app->SelectOne("SELECT count(*) as num from users where is_admin=false");
 $booking=$app->SelectOne("SELECT count(*) as num from bookings ");
+// table2=
+
 ?>
   <!-- Main Sidebar Container -->
  <?php require_once('layout/sidebar.php') ?>
@@ -79,7 +81,64 @@ $booking=$app->SelectOne("SELECT count(*) as num from bookings ");
             </div>
           </div>
           <!-- ./col -->
+          <!-- ========================================= -->
+          <div class="card-body">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Task</th>
+                      <th>Progress</th>
+                      <th style="width: 40px">Label</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Update software</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-danger">55%</span></td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>Clean database</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar bg-warning" style="width: 70%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-warning">70%</span></td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>Cron job running</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-primary" style="width: 30%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-primary">30%</span></td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>Fix and squish bugs</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-success" style="width: 90%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-success">90%</span></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+           
+          <!-- ========================================= -->
         </div>
   </div>
-  
+
   <?php include_once('layout/footer.php'); ?>
